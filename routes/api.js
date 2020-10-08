ee58e5d5145270af9ed693d14e86611dc151e5a0
Mevlut Canvar <mevlutcanvar@gmail.com>
2020-10-08 23:16:08 +0000

    v1.0.0

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,163 @@
 'use strict';
 
 var expect = require('chai').expect;
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
+const threadSchema = new mongoose.Schema(
+  {
+    text: String,
+    created_on: Date,
+    bumped_on: Date,
+    reported: Boolean,
+    delete_password: String,
+    board: { type: mongoose.Schema.Types.ObjectId, ref: 'Board' },
+    replies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reply' }]
+  }
+);
+const replySchema = new mongoose.Schema(
+  {
+    text: String,
+    created_on: Date,
+    reported: Boolean,
+    delete_password: String
+  }
+);
+const boardSchema = new mongoose.Schema(
+  {
+    name: String
+  }
+);
+const Thread = mongoose.model('Thread', threadSchema);
+const Reply = mongoose.model('Reply', replySchema);
+const Board = mongoose.model('Board', boardSchema);
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+
+    .get( async (req, res) => {
+      const board = await Board.findOne({name: req.params.board}).exec();
+
+      Thread.find({board: board._id}, (error, data) => {
+        if(error) console.log(error);
+
+        res.json(data);
+      })
+      .populate({path: 'replies', model: Reply, options: {limit: 3}})
+      .limit(10)
+      .sort({bumped_on: 'desc'})
+      .select({reported: false, delete_password: false, board: false});
+    })
+
+    .post(async (req, res) => {
+      let now = new Date();
+
+      let board = await Board.findOne({name: req.params.board}).exec();
+
+      if(board === null) 
+        board = await Board.create({name: req.params.board});
+
+      let thread = new Thread({
+        text: req.body.text,
+        created_on: now,
+        bumped_on: now,
+        reported: false,
+        delete_password: req.body.delete_password,
+        board: board._id,
+        replies: []
+      });
+
+      thread.save((error, data) => {
+          if(error) console.log(error);
+          
+          res.redirect('/b/' + board.name);
+      });
+    })
+
+    .put((req, res) => {
+
+      Thread.findByIdAndUpdate(req.body.thred_id, {reported: true}, (error, data) => {
+        if(error) console.log(error);
+          
+        res.send('success');
+      });
+
+    })
+
+    .delete(async (req, res) => {
+      const thread = await Thread.findById(req.body.thread_id).exec();
+      if(thread.delete_password !== req.body.delete_password){
+        res.send('incorrect password');
+
+        return;
+      }
+
+      await Thread.findByIdAndDelete(thread._id).exec();
+      
+      res.send('success');
+    });
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+  
+    .get(async (req, res) => {
+      
+      let thread = await Thread.findById(
+        req.query.thread_id, 
+      )
+      .populate({ path: 'replies', model: Reply })
+      .select({reported: false, delete_password: false, board: false}).exec((error, data) => {
+        if(error) console.log(error);
+
+        res.json(data);
+      });
+    })
+
+    .post((req, res) => {
+
+      let reply = new Reply({
+        text: req.body.text,
+        created_on: new Date(),
+        reported: false,
+        delete_password: req.body.delete_password
+      });
+
+      reply.save((error, replyData) => {
+          if(error) console.log(error);
+          
+          Thread.findByIdAndUpdate(
+            req.body.thread_id, 
+            {$push: { replies: replyData._id }},
+            (error, data) => {
+              if(error) console.log(error);
+
+              res.redirect('/b/' + req.params.board + '/' + req.params.threadid);
+          });
+      });
+    })
+
+    .put((req, res) => {
+
+      Reply.findByIdAndUpdate(req.body.reply_id, {reported: true}, (error, data) => {
+        if(error) console.log(error);
+          
+        res.send('success');
+      });
+
+    })
+
+    .delete(async (req, res) => {
+      const reply = await Reply.findById(req.body.reply_id).exec();
+
+      if(reply.delete_password !== req.body.delete_password){
+        res.send('incorrect password');
+
+        return;
+      }
+
+      await Reply.findByIdAndDelete(reply._id).exec();
+      
+      res.send('success');
+    });
 
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,21 +15,106 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+  let threadOne;
+  let threadTwo;
+  let reply;
+  let text = ' Hi';
+
   suite('API ROUTING FOR /api/threads/:board', function() {
     
     suite('POST', function() {
       
+      test('Create 2 new threads', done => {
+        chai.request(server)
+          .post('/api/threads/app-test')
+          .send({
+            text: 'Test',
+            delete_password: 'secret'
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+          });
+        chai.request(server)
+          .post('/api/threads/app-test')
+          .send({
+            text: 'Test 2',
+            delete_password: 'secret'
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
     });
     
     suite('GET', function() {
-      
+
+      test('List threads', done => {
+        chai.request(server)
+          .get('/api/threads/app-test')
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.isArray(res.body, 'response should be an array');
+            assert.property(res.body[0], '_id');
+            assert.property(res.body[0], 'created_on');
+            assert.property(res.body[0], 'bumped_on');
+            assert.property(res.body[0], 'text');
+            assert.property(res.body[0], 'replies');
+            assert.isArray(res.body[0].replies);
+            assert.isBelow(res.body[0].replies.length, 4);
+            threadOne = res.body[0]._id;
+            threadTwo = res.body[1]._id;
+            done();
+          });
+      });
+
     });
     
     suite('DELETE', function() {
-      
+
+      test('correct password', done => {
+        chai.request(server)
+        .delete('/api/threads/app-test')
+        .send({
+          thread_id: threadOne, 
+          delete_password:'secret'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+      });
+
+      test('wrong password', done => {
+        chai.request(server)
+        .delete('/api/threads/app-test')
+        .send({
+          thread_id: threadTwo, 
+          delete_password:'known'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'incorrect password');
+          done();
+        });
+      });
     });
     
     suite('PUT', function() {
+
+      test('Report a thread', done => {
+        chai.request(server)
+        .put('/api/threads/app-test')
+        .send({
+          report_id: threadTwo
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+      });
       
     });
     
@@ -40,17 +125,98 @@ suite('Functional Tests', function() {
     
     suite('POST', function() {
       
+      test('reply to thread', done => {
+        chai.request(server)
+        .post('/api/replies/app-test')
+        .send({
+          thread_id: threadTwo, 
+          text:'a reply' + text, 
+          delete_password:'secret'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+
+          done();
+        });
+      });
+      
     });
     
     suite('GET', function() {
+      
+      test('Get all replies', done => {
+        chai.request(server)
+        .get('/api/replies/app-test')
+        .query({
+          thread_id: threadOne
+          })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body, '_id');
+          assert.property(res.body, 'created_on');
+          assert.property(res.body, 'bumped_on');
+          assert.property(res.body, 'text');
+          assert.property(res.body, 'replies');
+          assert.isArray(res.body.replies);
+          assert.property(res.body.replies[0], 'delete_password');
+          assert.property(res.body.replies[0], 'reported');
+          assert.equal(res.body.replies[0].text, 'a reply' + text);
+          reply = res.body.replies[0]._id;
+          done();
+        });
+      });
       
     });
     
     suite('PUT', function() {
       
+      test('Report a reply', done => {
+        chai.request(server)
+        .put('/api/replies/app-test')
+        .send({
+          thread_id: threadOne,
+          reply_id: reply
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+      });
+      
     });
     
     suite('DELETE', function() {
+      
+      test('Delete reply with wrong password', done => {
+        chai.request(server)
+        .delete('/api/replies/app-test')
+        .send({
+          thread_id: threadOne,
+          reply_id: reply,
+          delete_password: 'known'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'incorrect password');
+          done();
+        });
+      });
+      
+      test('delete reply with valid password', done => {
+        chai.request(server)
+        .delete('/api/replies/app-test')
+        .send({
+          thread_id: threadOne,
+          reply_id: reply,
+          delete_password: 'secret'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+      });
       
     });
     
